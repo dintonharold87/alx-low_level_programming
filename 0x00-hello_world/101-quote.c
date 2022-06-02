601733d6ebbe5565f6799d76101c64c0f6c6dff3
Dinton Harold <dintonharold@gmail.com>
2022-06-02 22:09:37 +0000

    Made changes to file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - Entry point
-* Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
-* Return: Always 0 (Success)
+* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* Return: Always 1.
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,");
-fprintf(stderr, "2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }

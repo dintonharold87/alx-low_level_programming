a8af8da04f2e9d9583df11aaf34f3625f36baa96
Dinton Harold <dintonharold@gmail.com>
2022-06-02 13:38:41 +0000

    Added file 6 and it's output files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = sizeof(char);
+int y = sizeof(int);
+int z = sizeof(long int);
+int a = sizeof(long long int);
+int b = sizeof(float);
+printf("Size of a char: %d byte(s)\n", x);
+printf("Size of an int: %d byte(s)\n", y);
+printf("Size of a long int: %d byte(s)\n", z);
+printf("Size of a long long int: %d byte(s)\n", a);
+printf("Size of a float: %d byte(s)\n", b);
+return (0);
+}

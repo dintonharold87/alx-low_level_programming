2ebabf99a1245e7894b40b276c362f5fa1998022
Dinton Harold <dintonharold@gmail.com>
2022-06-03 10:57:34 +0000

    Added positive_or_negative file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* Return: Always 1.
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is a positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+/* your code goes there */
+return (0);
+}

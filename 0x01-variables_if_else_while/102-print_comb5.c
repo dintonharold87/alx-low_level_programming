70f2ce35d74e47a56cf3cf5738b6e3a8a4211ebb
Dinton Harold <dintonharold@gmail.com>
2022-06-05 18:34:14 +0000

    Updated file 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,22 @@ for (i = 48; i <= 57; i++)
 {
 for (j = 48; j <= 57; j++)
 {
-for (k = 48; k <= 57; k++)
+for (k = i; k <= 57; k++)
 {
-for (l = k + 1; l <= 57; l++)
+for (l = j + 1; l <= 57; l++)
 {
-putchar(i);
-putchar(j);
-putchar(' ');
-putchar(k);
-putchar(l);
-putchar(',');
-putchar(' ');
+	putchar(i);
+	putchar(j);
+	putchar(' ');
+	putchar(k);
+	putchar(l);
+if ((i != 57) || (j != 56) || (k != 57) || (l != 57))
+{
+	putchar(',');
+	putchar(' ');
 }
+}
+l = 48;
 }
 }
 }

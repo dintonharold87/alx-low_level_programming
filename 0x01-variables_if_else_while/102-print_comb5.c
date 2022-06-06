784c4b27182074d6cbc8b1f7fbbf248d31826330
Dinton Harold <dintonharold@gmail.com>
2022-06-06 09:37:17 +0000

    Updated file 102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,29 +5,21 @@
 */
 int main(void)
 {
-int i, j, k, l;
-for (i = 48; i <= 57; i++)
+int i, j;
+for (i = 0; i <= 98; i++)
 {
-for (j = 48; j <= 57; j++)
-{
-for (k = i; k <= 57; k++)
-{
-for (l = j + 1; l <= 57; l++)
-{
-	putchar(i);
-	putchar(j);
-	putchar(' ');
-	putchar(k);
-	putchar(l);
-if ((i != 57) || (j != 56) || (k != 57) || (l != 57))
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-l = 48;
-}
-}
+	for (j = i + 1; j <= 99; j++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		if ((i == 98) && (j == 99))
+			break;
+		putchar(',');
+		putchar(' ');
+	}
 }
 putchar('\n');
 return (0);

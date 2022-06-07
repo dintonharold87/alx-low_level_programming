4a9193bf1bf5199ecdb3936ffea59d70d125a983
Dinton Harold <dintonharold@gmail.com>
2022-06-07 20:12:29 +0000

    Added file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - prints the sum of two integers.
+* @a: first integer argument.
+* @b: second integer argument.
+* Return: The sum of the arguments.
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}

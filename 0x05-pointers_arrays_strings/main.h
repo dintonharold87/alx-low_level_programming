7c7c63a52acd31317c54844e96bd50a573446127
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-13 10:32:36 +0300

    Created a README.md file and main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  Header file for my function prototypes
+ *
+ *        Version:  1.0
+ *        Created:  06/13/2022 10:29:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dinton Harold Ainemukama (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+int _putchar(char c);
+

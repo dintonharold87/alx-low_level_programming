85e3bc58813e958c45e7a38a16b7e7ce4df9bf0d
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-13 17:34:03 +0300

    updated file 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,7 @@ void puts2(char *str)
 	while (str[i] != '\0')
 		i++;
 	length = i - 1;
-	for (j = 0; j <= length; j++)
-	{
-		if ((str[j] % 2) == 0)
-			_putchar(str[j]);
-		else
-			continue;
-	}
+	for (j = 0; j <= length; j += 2)
+		_putchar(str[j]);
 	_putchar('\n');
 }

a4b19eb9200ac1dcc6c5ffb2b847ac5923dba082
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-13 17:19:52 +0300

    Added file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+/**
+* puts2-prints the characters of a string starting from the first character
+*@str: string whose characters are printed
+*/
+#include "main.h"
+void puts2(char *str)
+{
+	int i, j, length;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	length = i - 1;
+	for (j = 0; j <= length; j++)
+	{
+		if ((str[j] % 2) == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}

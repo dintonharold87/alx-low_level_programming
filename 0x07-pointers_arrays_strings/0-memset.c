b9c8a09f892fc8c186a46cb28f7c3af0a38e70a8
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-20 13:19:46 +0300

    Added file 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memset- starting address of the memory to be filled
+* @s:Starting address of memory to be filled
+* @b:Value to be filled
+* @n:Number of bytes to be filled starting from ptr to be filled
+* Return:a pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}

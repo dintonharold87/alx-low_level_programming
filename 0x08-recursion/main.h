51c34b83c1172731ce9a843f1b442a64280fa6c2
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-21 21:57:06 +0300

    Created directory 0x08-recursion,added a README.md file, main.h header file with all function prototypes

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description: Header file containing all function prototypes 
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2022 10:34:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dinton Harold Ainemukama (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);

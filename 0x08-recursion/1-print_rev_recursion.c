ecf9a4a9132b4b99a0c2f40a1bbcee88ac10d206
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-22 12:54:36 +0300

    updated file 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,9 +5,11 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }

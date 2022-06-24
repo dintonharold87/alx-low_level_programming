760a1bc202afc1037956ab0b6caf344d1bc57a91
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-24 09:07:54 +0300

    Added file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description: Header file containing all function prototypes 
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2022 10:34:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dinton Harold Ainemukama (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+int _putchar(char c);
+

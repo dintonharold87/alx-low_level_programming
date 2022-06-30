05afdfe742f4e8eeb6d928939668e15dfb39d09e
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-01 00:24:13 +0300

    Updated file 101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -24,5 +24,10 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif

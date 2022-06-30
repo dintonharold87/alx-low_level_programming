60982d003153de3ab1d147f47787f6bfcac0d283
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-06-30 12:13:08 +0300

    Added file 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description: Header file containing all function prototypes 
+ *
+ *        Version:  1.0
+ *        Created:  06/15/2022 10:34:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dinton Harold Ainemukama (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+#endif

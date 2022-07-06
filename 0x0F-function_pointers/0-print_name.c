243a1b8438b7ce12651d2b70f0c536acea0e0657
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-06 11:20:54 +0300

    Added file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+* print_name-function that prints a name
+* @name:the name to be printed
+* @f:function pointer to the function to be called
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}

f837273c8c7d4dba2021584ed4432a6ad1d1ab72
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-09-05 16:17:27 +0300

    Updated 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp
-https://github.com/dintonharold87/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/dintonharold87/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
